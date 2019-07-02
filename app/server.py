ee81ad30d65a0d2d8a868fa9ad8dda0dcc29bcbb
Omar Khursheed <omarkhu@gmail.com>
2019-07-02 12:25:45 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,23 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/uc?export=download&id=15a2DYsYX3hTY3vc_02b1YyijJSPYywia'
+export_file_name = 'learn3.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['abstract',
+ 'animal-painting',
+ 'cityscape',
+ 'figurative',
+ 'flower-painting',
+ 'genre-painting',
+ 'landscape',
+ 'marina',
+ 'mythological-painting',
+ 'nude-painting-nu',
+ 'portrait',
+ 'religious-painting',
+ 'still-life',
+ 'symbolic-painting']
 path = Path(__file__).parent
 
 app = Starlette()
